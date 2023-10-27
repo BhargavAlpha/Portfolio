ce94c971e6f06952d27ded2a02811b9663faa22d
Bhargav <bhargavvenkat515@gmail.com>
2023-10-27 17:02:17 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter} from 'react-router-dom';
+
+//import {About, Contact ,Experience, Feedback, Navbar,Tech,Projects,StarsCanvas} from './components';
+
+const App=()=>{
+  return()=>{
+    <div>
+      3D PORTFOLIO
+      
+    </div>
+  }
+}
+export default App;
